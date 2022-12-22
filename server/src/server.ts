7af99aa5bede1d2c98086569e7dd4140aa6c235c
Luiz Victor  Nascimento Andrade <lvictor@digiboard.com.br>
2022-12-22 13:42:06 -0400

    Adjust the PORT variable on env file

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,4 +13,4 @@ app.use(express.json());
 app.use(routes)
 
 
-app.listen(process.env.port || 3333, () => console.log("Server is running!"));
+app.listen(process.env.PORT || 3333, () => console.log("Server is running!"));
